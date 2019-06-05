ba3626d6f54c688b24d6ab5be509150dcd7ca800
Hollis Ma <hollism@princeton.edu>
2019-06-05 15:17:59 -0400

    updated server.js

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ if (process.env.NODE_ENV === 'production') {
 	// Set static folder
 	app.use(express.static('client/build'));
 
-	console.log('hello');
-
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
