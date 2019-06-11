a578d1e474092e69d16da68867621e28bf84ad42
Hollis Ma <hollism@princeton.edu>
2019-06-10 21:54:35 -0400

    added auth and error reducers and actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,7 @@ import { combineReducers } from 'redux';
 import itemReducer from './itemReducer';
 
 export default combineReducers({
-    item: itemReducer
+    item: itemReducer,
+    error: errorReducer,
+    auth: authReducer
 });
